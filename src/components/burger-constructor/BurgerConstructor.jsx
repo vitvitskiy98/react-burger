c2c4e0cac3d1dd90dbb56d8c0a24dd43126e712e
Vlad <vladyn19981998@gmail.com>
2023-06-28 20:28:08 -0500

    fixed the scroll styles

--- src/components/burger-constructor/BurgerConstructor.jsx
+++ src/components/burger-constructor/BurgerConstructor.jsx
@@ -4,21 +4,19 @@ import burgerConstructorStyle from './burger-constructor.module.css'
 import { BurgerConstructorIngredient } from '../burger-constructor-ingredient/BurgerConstructorIngredient';
 import { DragIcon, CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components';
 export const BurgerConstructor = () => {
-    let total = Array.from(document.querySelectorAll('span.constructor-element__price'))
-        .map(elem => Number(elem.innerText))
-        .reduce((acc, cur) => acc + cur, 0);
-    // не работает в react, но работает в консоли
+
     let total2 = [20, 30, 300, 80, 80, 80, 20].reduce((acc, cur) => acc + cur, 0);
 
     return (
-        <div className={`${burgerConstructorStyle.block} ${burgerConstructorStyle.styledScrollbars } mt-25`}>
+        <div className={`${burgerConstructorStyle.block} mt-25`}>
             <div className={`${burgerConstructorStyle.ingredientsBlock}`}>
             <BurgerConstructorIngredient type="top"
                 isLocked={true}
                 text="Краторная булка N-200i (верх)"
                 price={20}
                 thumbnail={data[0].image} />
-            
+
+            <div className={`${burgerConstructorStyle.chosenIngredients}`}>
             <BurgerConstructorIngredient type="top"
                 text={data[5].name}
                 price={30}
@@ -53,6 +51,7 @@ export const BurgerConstructor = () => {
                 thumbnail={data[8].image} >
                 <DragIcon type="primary" />
             </BurgerConstructorIngredient>
+            </div>
 
             <BurgerConstructorIngredient type="top"
                 isLocked={true}
--- src/components/burger-ingrediens/BurgerIngredients.jsx
+++ src/components/burger-ingrediens/BurgerIngredients.jsx
@@ -7,11 +7,13 @@ import { FillingCategory } from "../categories/filling-category/FillingCategory"
 export const BurgerIngredients = () => {
   return (
     <div className={`${burgerIngredientsStyle.burgerIngredients} ml-85`}>
-    <h2 className={`${burgerIngredientsStyle.burgerIngredientsHeading} mt-10`}>Соберите бургер</h2>
-    <Tabs />
-    <BurgerCategory/>
-    <SauceCategory/>
-    <FillingCategory/>
+      <h2 className={`${burgerIngredientsStyle.burgerIngredientsHeading} mt-10`}>Соберите бургер</h2>
+      <Tabs />
+      <div className={`${burgerIngredientsStyle.categories}`}>
+        <BurgerCategory />
+        <SauceCategory />
+        <FillingCategory />
+      </div>
     </div>
   );
 };
