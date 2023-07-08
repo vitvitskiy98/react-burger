8efe841f624af5f0b700034069c0ffdc5184f7f0
Vlad <vladyn19981998@gmail.com>
2023-07-08 10:13:21 -0500

    made the order modal dynamic

--- src/components/burger-constructor/BurgerConstructor.jsx
+++ src/components/burger-constructor/BurgerConstructor.jsx
@@ -1,16 +1,29 @@
-import React, { useState, useMemo, useContext } from "react";
+import React, {
+  useState,
+  useEffect,
+  useMemo,
+  useContext,
+  useCallback,
+} from "react";
 import burgerConstructorStyle from "./burger-constructor.module.css";
 // import orderDetailsStyle from "./orderDetails.module.css";
 import { BurgerConstructorIngredient } from "../burger-constructor-ingredient/BurgerConstructorIngredient";
 import Modal from "../modals/Modal";
+import { v4 as uuidv4 } from "uuid";
 import {
   DragIcon,
   CurrencyIcon,
-  Button,ConstructorElement
+  Button,
+  ConstructorElement,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import OrderDetails from "./OrderDetails";
 import { dataContext } from "../services/dataContext";
 export const BurgerConstructor = () => {
+  const [state, setState] = useState({
+    isLoading: false,
+    hasError: false,
+    orders: [],
+  });
   const { ...dataState } = useContext(dataContext);
   const [openModal, setOpenModal] = useState(false);
   const bun = dataState.data.filter((el) => el.type === "bun")[0];
@@ -21,7 +34,7 @@ export const BurgerConstructor = () => {
       dataState.data[5],
       dataState.data[11],
       dataState.data[10],
-      dataState.data[10]
+      dataState.data[10],
     ],
     [dataState]
   );
@@ -30,47 +43,91 @@ export const BurgerConstructor = () => {
     () =>
       filteredData
         .map((elem, index) => elem?.price)
-        .reduce((acc, cur) => acc + cur, (bun?.price) *2),
+        .reduce((acc, cur) => acc + cur, bun?.price * 2),
     [filteredData, bun?.price]
   );
 
   const { isLoading, hasError } = dataState;
 
+  const fetchPostData = useCallback(() => {
+    const ingredients = [
+      "643d69a5c3f7b9001cfa0944",
+      "643d69a5c3f7b9001cfa093f",
+      "643d69a5c3f7b9001cfa0947",
+      "643d69a5c3f7b9001cfa0946",
+      "643d69a5c3f7b9001cfa0946",
+    ];
+    setState({ ...state, hasError: false, isLoading: true });
+    try {
+      fetch("https://norma.nomoreparties.space/api/orders", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify({ ingredients }),
+      })
+        .then((res) => {
+          return res.json();
+          // eslint-disable-next-line no-unreachable
+          if (!res.ok) {
+            throw new Error("Ответ сети был не ok.");
+          }
+        })
+        .then((json) =>
+          setState({
+            loading: false,
+            status: "success",
+            orders: json,
+          })
+        )
+        .catch((e) => {
+          setState({ ...state, hasError: true, isLoading: false });
+        });
+    } catch (error) {
+      console.log("Возникла проблема с вашим fetch запросом: ", error.message);
+    }
+  }, [state]);
+
+  useEffect(() => {
+    fetchPostData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+ 
   return (
     <div className={`${burgerConstructorStyle.block} mt-25`}>
       <div className={`${burgerConstructorStyle.ingredientsBlock}`}>
-      <ConstructorElement
-            type="top"
-            isLocked={bun?.isLocked}
-            text={`${bun?.name} (верх)`}
-            price={bun?.price}
-            thumbnail={bun?.image}
-          /> 
-          </div>
-        <div className={`${burgerConstructorStyle.chosenIngredients}`}>
-          {isLoading && "Загрузка..."}
-          {hasError && "Произошла ошибка"}
-          {!isLoading &&
-            !hasError &&
-            filteredData.length &&
-            filteredData.map((elem, index) =>
-                <BurgerConstructorIngredient
-                  key={index}
-                  text={elem?.name}
-                  price={elem?.price}
-                  thumbnail={elem?.image}
-                  children={<DragIcon type="primary" />}
-                />
-            )}
-        </div>
-        <div className={`${burgerConstructorStyle.ingredientsBlock}`}>
         <ConstructorElement
-            type="top"
-            isLocked={bun?.isLocked}
-            text={`${bun?.name} (низ)`}
-            price={bun?.price}
-            thumbnail={bun?.image}
-          /> 
+          type="top"
+          isLocked={bun?.isLocked}
+          text={`${bun?.name} (верх)`}
+          price={bun?.price}
+          thumbnail={bun?.image}
+        />
+      </div>
+      <div className={`${burgerConstructorStyle.chosenIngredients}`}>
+        {isLoading && "Загрузка..."}
+        {hasError && "Произошла ошибка"}
+        {!isLoading &&
+          !hasError &&
+          filteredData.length &&
+          filteredData.map((elem, index) => (
+            <BurgerConstructorIngredient
+              key={uuidv4()}
+              text={elem?.name}
+              price={elem?.price}
+              thumbnail={elem?.image}
+              children={<DragIcon type="primary" />}
+            />
+          ))}
+      </div>
+      <div className={`${burgerConstructorStyle.ingredientsBlock}`}>
+        <ConstructorElement
+          type="top"
+          isLocked={bun?.isLocked}
+          text={`${bun?.name} (низ)`}
+          price={bun?.price}
+          thumbnail={bun?.image}
+        />
       </div>
       <div className={`${burgerConstructorStyle.totalBlock} mt-10`}>
         <div className={`${burgerConstructorStyle.total}`}>
@@ -88,7 +145,7 @@ export const BurgerConstructor = () => {
         </Button>
 
         <Modal open={openModal} closeModal={() => setOpenModal(false)}>
-          <OrderDetails />
+          <OrderDetails number={state.orders?.order?.number} />
         </Modal>
       </div>
     </div>
